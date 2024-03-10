2882e74a0754f6b254cf892b0b9e4c1ab8c00115
domas <domas.guiga@gmail.com>
2024-03-10 20:04:16 +0200

    api update

--- app/api/targets/route.ts
+++ app/api/targets/route.ts
@@ -1,18 +1,24 @@
 import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import { open, Database } from "sqlite";
+
+let db: Database<sqlite3.Database, sqlite3.Statement>;
 
 export async function GET(req: Request, res: Response) {
     
-    const { searchParams } = new URL(req.url!);
+    const { searchParams } = new URL(req.url);
     const column = searchParams.get("columnName");
     const query = `SELECT DISTINCT ${column} FROM irisdata;`;
-
-    const db = await open({
-        filename: "./datasets.db",
-        driver: sqlite3.Database,
-    });
+    
+    if (!db) {
+        db = await open({
+            filename: "./datasets.db",
+            driver: sqlite3.Database,
+        });
+    }
 
     const items = await db.all(query);
+
+    console.log(items);
 
     return new Response(JSON.stringify(items), {
         headers: { "Content-Type": "application/json" },
