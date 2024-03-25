b26cf2be5f1bbb332f20dff753b5b81b51ffc670
domas <domas.guiga@gmail.com>
2024-03-25 16:32:05 +0200

    fallback loading screen update

--- app/ui/misc/skeletons.tsx
+++ app/ui/misc/skeletons.tsx
@@ -2,12 +2,12 @@ export const DatasetPanelSkeleton = () => {
     return (
         <>
             <div className="my-2">
-                <div className="rounded-full h-8 px-4 bg-slate-200">
+                <div className="rounded-full h-8 px-4 bg-slate-200 dark:bg-slate-700">
                     <div className="w-16"></div>
                 </div>
             </div>
             <div className="my-2">
-                <div className="rounded-full h-8 px-4 bg-slate-200">
+                <div className="rounded-full h-8 px-4 bg-slate-200 dark:bg-slate-700">
                     <div className="w-16"></div>
                 </div>
             </div>
@@ -17,10 +17,10 @@ export const DatasetPanelSkeleton = () => {
 
 export const DatasetSkeleton = () => {
     return (
-        <div className="flex shadow-sm h-full">
+        <div className="bg-white flex rounded-md shadow-sm h-full border dark:bg-slate-800 dark:border-slate-700">
             <div className="py-5 px-6 w-full">
                 <div className="flex justify-between">
-                    <div className="text-base font-bold uppercase">
+                    <div className="text-base font-bold uppercase dark:text-teal-100">
                         Dataset
                     </div>
                 </div>
@@ -34,26 +34,26 @@ export const DatasetSkeleton = () => {
 
 export const ParametersSkeleton = () => {
     return (
-        <div className="h-full rounded-xl shadow-sm">
+        <div className="h-full rounded-md shadow-sm bg-white border dark:bg-slate-800 dark:border-slate-700">
             <div className="flex flex-col py-5 px-1 w-full h-full">
                 <div className="mx-5 shrink">
-                    <div className="bg-white text-base font-bold uppercase text-ellipsis overflow-hidden" title="Hyperparameters">
+                    <div className="text-base font-bold uppercase text-ellipsis overflow-hidden dark:text-teal-100" title="Hyperparameters">
                         Hyperparameters
                     </div>
                 </div>
                 <div className="mx-1 mt-2 overflow-y-auto h-full animate-pulse">
                     <div className="mx-4 scrollable-container">
                         <div className="py-3">
-                            <div className="rounded-full w-1/2 h-4 bg-slate-200 mb-2"></div>
-                            <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10"></div>
+                            <div className="rounded-full w-1/2 h-4 bg-slate-200 mb-2 dark:bg-slate-700"></div>
+                            <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10 dark:bg-slate-700"></div>
                         </div>
                         <div className="py-4">
-                            <div className="rounded-full w-1/2 h-4 bg-slate-200 mb-2"></div>
-                            <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10"></div>
+                            <div className="rounded-full w-1/2 h-4 bg-slate-200 mb-2 dark:bg-slate-700"></div>
+                            <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10 dark:bg-slate-700"></div>
                         </div>
                         <div className="py-4">
-                            <div className="rounded-full w-1/2 h-4 bg-slate-200 mb-2"></div>
-                            <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10"></div>
+                            <div className="rounded-full w-1/2 h-4 bg-slate-200 mb-2 dark:bg-slate-700"></div>
+                            <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10 dark:bg-slate-700"></div>
                         </div>
                     </div>
                 </div>
@@ -64,10 +64,10 @@ export const ParametersSkeleton = () => {
 
 export const ModelsSkeleton = () => {
     return (
-        <div className="flex shadow-sm h-full">
+        <div className="flex shadow-sm rounded-md h-full bg-white border dark:bg-slate-800 dark:border-slate-700">
             <div className="py-5 px-6 w-full">
                 <div className="flex justify-between">
-                    <div className="text-base font-bold uppercase">
+                    <div className="text-base font-bold uppercase dark:text-teal-100">
                         Models
                     </div>
                 </div>
@@ -81,31 +81,31 @@ export const ModelsSkeleton = () => {
 
 export const PlaygroundSkeleton = () => {
     return (
-        <div className="w-full h-full flex flex-row gap-2 justify-center items-center">
+        <div className="w-full h-full flex flex-row gap-2 justify-center items-center bg-white rounded-md border dark:bg-slate-800 dark:border-slate-700">
             <div
                 className="inline-block h-6 w-6 border-slate-300 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
                 role="status"
             >
             </div>
-            <div className="text-slate-500 text-sm font-semibold uppercase tracking-wider">Loading...</div>
+            <div className="text-slate-500 text-sm font-semibold uppercase tracking-wider dark:text-slate-300">Loading...</div>
         </div>
     );
 }
 
 export const InfoMenuSkeleton = () => {
     return (
-        <div className="h-full rounded-xl shadow-sm">
+        <div className="h-full rounded-md shadow-sm bg-white border dark:bg-slate-800 dark:border-slate-700">
             <div className="flex flex-col py-5 px-1 w-full h-full">
                 <div className="mx-5 shrink">
-                    <div className="bg-white text-base font-bold uppercase text-ellipsis overflow-hidden" title="Hyperparameters">
+                    <div className="text-base font-bold uppercase text-ellipsis overflow-hidden dark:text-teal-100" title="Hyperparameters">
                         Network
                     </div>
                 </div>
                 <div className="mx-1 mt-2 overflow-y-auto h-full animate-pulse">
                     <div className="mx-4 scrollable-container">
-                        <div className="py-3 bg-white">
-                            <div className="rounded-full w-1/2 h-4 bg-slate-200 mb-2"></div>
-                            <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10"></div>
+                        <div className="py-3">
+                            <div className="rounded-full w-1/2 h-4 bg-slate-200 mb-2 dark:bg-slate-700"></div>
+                            <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10 dark:bg-slate-700"></div>
                         </div>
                     </div>
                 </div>
@@ -117,19 +117,19 @@ export const InfoMenuSkeleton = () => {
 
 export const LayersSkeleton = () => {
     return (
-        <div className="h-full rounded-xl shadow-sm">
+        <div className="h-full rounded-md shadow-sm bg-white border dark:bg-slate-800 dark:border-slate-700">
             <div className="flex flex-col py-5 px-1 w-full h-full">
                 <div className="mx-5 shrink">
-                    <div className="bg-white text-base font-bold uppercase text-ellipsis overflow-hidden" title="Hyperparameters">
+                    <div className="text-base font-bold uppercase text-ellipsis overflow-hidden dark:text-teal-100" title="Hyperparameters">
                         Layers
                     </div>
                 </div>
                 <div className="mx-5 mt-2 animate-pulse">
-                    <div className="py-1 bg-white">
-                        <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10"></div>
+                    <div className="py-1">
+                        <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10 dark:bg-slate-700"></div>
                     </div>
-                    <div className="py-1 bg-white">
-                        <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10"></div>
+                    <div className="py-1">
+                        <div className="bg-slate-100 py-2.5 px-4 rounded-lg w-full h-10 dark:bg-slate-700"></div>
                     </div>
                 </div>
             </div>
@@ -139,7 +139,7 @@ export const LayersSkeleton = () => {
 
 export const TrainButtonSkeleton = () => {
     return (
-        <button className="bg-slate-300 p-2 m-1 h-10 rounded-md animate-pulse"></button>
+        <button className="bg-slate-300 p-2 m-1 h-10 rounded-md animate-pulse dark:bg-slate-600"></button>
     );
 }
 
@@ -147,24 +147,24 @@ export function ModelPageSkeleton() {
     return (
         <div className="basis-11/12 flex flex-row gap-2 justify-stretch grow">
             <div className="basis-1/6 flex flex-col gap-2 max-w-56 min-w-48">
-                <div className="basis-1/6 bg-white rounded-lg shadow-xs border">
+                <div className="basis-1/6">
                     <DatasetSkeleton />
                 </div>
-                <div className="basis-2/3 bg-white rounded-lg shadow-xs border">
+                <div className="basis-2/3">
                     <ParametersSkeleton />
                 </div>
-                <div className="basis-1/6 bg-white rounded-lg shadow-xs border">
+                <div className="basis-1/6">
                     <ModelsSkeleton />
                 </div>
             </div>
-            <div className="basis-2/3 bg-white rounded-lg shadow-xs grow border">
+            <div className="basis-2/3 grow">
                 <PlaygroundSkeleton />
             </div>
             <div className="basis-1/6 flex flex-col gap-2 max-w-56 min-w-48">
-                <div className="basis-1/2 bg-white rounded-lg shadow-xs border">
+                <div className="basis-1/2">
                     <InfoMenuSkeleton />
                 </div>
-                <div className="basis-5/12 bg-white rounded-lg shadow-xs grow border">
+                <div className="basis-5/12 grow">
                     <LayersSkeleton />
                 </div>
                 <TrainButtonSkeleton />
