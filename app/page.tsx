714000d70a357197723432862acd1afe85da52d9
domas <domas.guiga@gmail.com>
2024-03-28 16:21:26 +0200

    added dark mode to main page

--- app/page.tsx
+++ app/page.tsx
@@ -11,15 +11,15 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <main className="h-full grid grid-cols-8 grid-rows-10 p-4">
+        <main className="h-full grid grid-cols-8 grid-rows-10 p-4 dark:bg-slate-900">
             <div className="col-span-8 flex flex-row items-center justify-between p-8">
                 <Logo iconSize="large" archType="" />
                 <Settings />
             </div>
             <div className="col-start-2 col-span-2 row-span-9 row-start-2 p-4 flex flex-col justify-center gap-4">
                 <div className="my-4">
-                    <div className="text-4xl font-bold text-left">Neural networks made easy.</div>
-                    <div className="pt-2 text-lg text-gray-600 text-left">Choose a neural network to start building.</div>
+                    <div className="text-4xl font-bold text-left dark:text-white">Neural networks made easy.</div>
+                    <div className="pt-2 text-lg text-slate-600 text-left dark:text-slate-300">Choose a neural network to start.</div>
                 </div>
                 <div className="flex flex-col h-2/3 gap-6">
                     <Link href="/model">
@@ -30,7 +30,7 @@ export default function Home() {
                     </Link>
                 </div>
             </div>
-            <div className="col-span-4 row-span-9 row-start-2 bg-stone-100 rounded-xl p-4">
+            <div className="col-span-4 row-span-9 row-start-2 bg-slate-100 rounded-xl p-4 dark:bg-slate-800">
                 <div></div>
             </div>
         </main>
--- app/ui/buttons.tsx
+++ app/ui/buttons.tsx
@@ -6,17 +6,17 @@ interface LinkToPageButtonProps {
 
 export function LinkToPageButton({ archType }: LinkToPageButtonProps) {
     return (
-        <div className="flex flex-row items-center justify-center h-24 w-64 rounded-2xl select-none
-            bg-stone-100 shadow-lg shadow-gray-200 
-            hover:cursor-pointer hover:bg-blue-100
-            active:bg-blue-200 active:shadow-md">
-            <div className="flex flex-row items-center justify-center w-1/4">
-                <span className="material-symbols-outlined">
-                    workspaces
-                </span>
-            </div>
-            <div className="text-base max-w-44 font-semibold w-3/4 leading-5 uppercase pr-4">
+        <div className="flex flex-row items-center justify-center h-24 w-64 rounded-xl select-none border shadow-sm bg-slate-100
+            hover:cursor-pointer hover:bg-slate-200 active:bg-slate-300
+            dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-500"
+        >
+            <div className="text-base max-w-44 font-semibold leading-5 uppercase dark:text-white">
                 {archType}
+            </div>
+            <div className="flex flex-row items-center justify-center">
+                <span className="material-symbols-outlined dark:text-white">
+                    arrow_forward
+                </span>
             </div>
         </div>
     );
