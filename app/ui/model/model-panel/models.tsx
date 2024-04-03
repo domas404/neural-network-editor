cd10455365e8d7c31fa2067a87872c75fd180aee
domas <domas.guiga@gmail.com>
2024-04-03 14:36:17 +0300

    added 'add new model' menu

--- app/ui/model/model-panel/models.tsx
+++ app/ui/model/model-panel/models.tsx
@@ -5,6 +5,7 @@ import { RadioOption } from "@/app/ui/misc/list-options";
 import { changeModel } from "@/app/lib/redux/features/network-slice";
 import { useDispatch } from "react-redux";
 import { AppDispatch, useAppSelector } from "@/app/lib/redux/store";
+import { useState } from "react";
 
 interface modelInfo {
     id: string,
@@ -16,6 +17,57 @@ const modelNames: modelInfo[] = [
     { id: "mymodel", name: "My Model" }
 ];
 
+interface ModelMenuProps {
+    pos: {
+        x: number,
+        y: number
+    };
+}
+
+const ModelMenu = ({ pos }: ModelMenuProps) => {
+    const height = pos.y - 118;
+    return (
+        <div
+            className="absolute z-10 rounded-md w-48 flex border
+                bg-white dark:bg-slate-700 dark:border-slate-600"
+            style={{ left: pos.x, top: height }}
+        >
+            <div className="flex flex-col w-full m-1 text-black dark:text-white text-sm">
+                <div className="w-full p-2 rounded-md hover:cursor-pointer flex flex-row gap-2
+                    bg-white hover:bg-slate-200 active:bg-lightblue-200
+                    dark:bg-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-500">
+                    <div className="flex items-center">
+                        <span className="material-symbols-outlined md-20">
+                            upload
+                        </span>
+                    </div>
+                    <div>Upload from pc</div>
+                </div>
+                <div className="w-full p-2 rounded-md hover:cursor-pointer flex flex-row gap-2
+                    bg-white hover:bg-slate-200 active:bg-lightblue-200
+                    dark:bg-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-500">
+                    <div className="flex items-center">
+                        <span className="material-symbols-outlined md-20">
+                            add_circle
+                        </span>
+                    </div>
+                    <div>Create default</div>
+                </div>
+                <div className="w-full p-2 rounded-md hover:cursor-pointer flex flex-row gap-2
+                    bg-white hover:bg-slate-200 active:bg-lightblue-200
+                    dark:bg-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-500">
+                    <div className="flex items-center">
+                        <span className="material-symbols-outlined md-20">
+                            add_circle
+                        </span>
+                    </div>
+                    <div>Create from current</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default function Models() {
 
     const currentModel = useAppSelector((state) => state.networkReducer.modelId);
@@ -25,6 +77,26 @@ export default function Models() {
         dispatch(changeModel(event.currentTarget.value));
     }
 
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [buttonPosition, setButtonPosition] = useState({ x: 0, y: 0});
+
+    const closeModelMenu = () => {
+        document.removeEventListener('mouseup', closeModelMenu);
+        setMenuOpen(false);
+    }
+    
+    const openModelMenu = (event: React.MouseEvent<HTMLElement>) => {
+        if (!menuOpen) {
+            const left = event.currentTarget.offsetLeft;
+            const top = event.currentTarget.offsetTop;
+            const width = event.currentTarget.offsetWidth;
+            const height = event.currentTarget.offsetHeight;
+            setButtonPosition({ x: left + width, y: top + height/2 })
+            setMenuOpen(true);
+            document.addEventListener('mouseup', closeModelMenu);
+        }
+    }
+    
     return (
         <div className="bg-white flex rounded-md border shadow-sm h-full dark:bg-slate-800 dark:border-slate-700">
             <div className="py-5 px-6 w-full relative">
@@ -34,11 +106,14 @@ export default function Models() {
                     </div>
                     <div className="absolute right-4 top-4 flex p-1 rounded-full select-none hover:cursor-pointer
                         hover:bg-slate-200 active:bg-lightblue-200 transition-all ease-in-out duration-200
-                        dark:hover:bg-slate-700 dark:active:bg-slate-600">
+                        dark:hover:bg-slate-700 dark:active:bg-slate-600"
+                        onClick={openModelMenu}
+                    >
                         <span className="material-symbols-outlined text-slate-600 dark:text-slate-100">
                             add
                         </span>
                     </div>
+                    {menuOpen && <ModelMenu pos={buttonPosition} />}
                 </div>
                 <div className="overflow-x-scroll flex flex-row h-14 items-center gap-2">
                     {
