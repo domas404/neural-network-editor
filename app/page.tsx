d3ed54479d81ac26d6236f8256a634eaa1dbd670
domas <domas.guiga@gmail.com>
2024-05-22 23:13:29 +0300

    main page update

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import Logo from '@/app/ui/logo';
 import { LinkToPageButton } from '@/app/ui/buttons';
 import Link from 'next/link';
 import type { Metadata } from 'next';
-// import Image from "next/image";
-// import DefaultImage from "@/public/nn3.png";
+import AppUILight from "@/public/app-ui-light.png";
+import ModelUIDark from "@/public/app-ui-model-dark.png";
+import DataUIDark from "@/public/app-ui-data-dark.png";
+import Image from "next/image";
 
 export const metadata: Metadata = {
     title: 'Home | Neural Network Editor',
@@ -13,28 +15,35 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <main className="h-full grid grid-cols-8 grid-rows-10 p-4 dark:bg-slate-900">
-            <div className="col-span-8 flex flex-row items-center justify-between p-8">
+        <main className="h-full flex flex-col dark:bg-slate-900">
+            <div className="z-20 h-16 flex flex-row items-center justify-between p-4 m-4">
                 <Logo iconSize="large" archType="" />
                 <Settings />
             </div>
-            <div className="col-start-2 col-span-2 row-span-9 row-start-2 p-4 flex flex-col justify-center gap-4">
-                <div className="my-4">
-                    <div className="text-4xl font-bold text-left dark:text-white">Neural networks made easy.</div>
-                    <div className="pt-2 text-lg text-slate-600 text-left dark:text-slate-300">Choose a neural network to start.</div>
+            <div className="relative h-full w-full flex">
+                <div className="z-10 opacity-20 absolute pointer-events-none bg-slate-300 dark:bg-slate-700 h-full w-full"></div>
+                <div className="z-20 w-full h-full flex flex-row mx-48 items-center mb-20">
+                    <div className="w-1/2 min-w-96 flex flex-col justify-center">
+                        <div className="text-4xl font-bold text-left dark:text-white">Learn neural networks</div>
+                        <div className="pt-2 text-lg text-slate-600 text-left dark:text-slate-300 pr-12">
+                            Neural network editor is an educational tool for learning the basics of neural networks
+                            by visually creating and training models. Choose a neural network to begin.
+                        </div>
+                        <div className="z-20 flex flex-col gap-6 mt-6">
+                            <Link href="/model" >
+                                <LinkToPageButton archType="Multilayer Perceptron" archDesc="Classification of iris, penguin and wine datasets." />
+                            </Link>
+                            <Link href="/cnn" >
+                                <LinkToPageButton archType="Convolutional Neural Network" archDesc="Classification of MNIST handwritten digit dataset." />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="z-20 relative h-3/4 w-1/2">
+                        <Image src={DataUIDark} alt="" fill={true} className="-mt-8 pointer-events-none" style={{objectFit: "contain"}} />
+                        <Image src={ModelUIDark} alt="" fill={true} className="mt-12 ml-20 pointer-events-none" style={{objectFit: "contain"}} />
+                    </div>
                 </div>
-                <div className="flex flex-col h-2/3 gap-6">
-                    <Link href="/model" >
-                        <LinkToPageButton archType="Multilayer Perceptron" />
-                    </Link>
-                    <Link href="/cnn" >
-                        <LinkToPageButton archType="Convolutional Neural Network" />
-                    </Link>
-                </div>
-            </div>
-            <div className="relative col-span-4 row-span-8 row-start-2 bg-slate-100 rounded-xl p-4 dark:bg-slate-800">
-                {/* <Image src={DefaultImage} alt="" fill={true} className="rounded-xl" style={{objectFit: "cover"}} /> */}
-            </div>
+            </div>            
         </main>
     );
 }
--- app/ui/buttons.tsx
+++ app/ui/buttons.tsx
@@ -2,21 +2,27 @@ import "@/app/globalicons.css";
 
 interface LinkToPageButtonProps {
     archType: string,
+    archDesc: string
 }
 
-export function LinkToPageButton({ archType }: LinkToPageButtonProps) {
+export function LinkToPageButton({ archType, archDesc }: LinkToPageButtonProps) {
     return (
-        <div className="flex flex-row items-center justify-center h-24 w-64 rounded-xl select-none border shadow-sm bg-slate-100
-            hover:cursor-pointer hover:bg-slate-200 active:bg-slate-300
+        <div className="flex flex-col items-center justify-center h-32 w-80 rounded-xl select-none border shadow-sm bg-slate-100
+            hover:cursor-pointer hover:bg-slate-200 active:bg-slate-300 px-6
             dark:bg-slate-800 dark:border-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-500"
         >
-            <div className="text-base max-w-44 font-semibold leading-5 uppercase dark:text-white">
-                {archType}
+            <div className="flex flex-row items-center justify-between w-full">
+                <div className="text-base font-semibold leading-5 uppercase dark:text-white">
+                    {archType}
+                </div>
+                <div className="flex flex-row items-center justify-center">
+                    <span className="material-symbols-outlined dark:text-white">
+                        arrow_forward
+                    </span>
+                </div>
             </div>
-            <div className="flex flex-row items-center justify-center">
-                <span className="material-symbols-outlined dark:text-white">
-                    arrow_forward
-                </span>
+            <div className="text-slate-700 dark:text-slate-400 text-sm mt-1 w-full">
+                {archDesc}
             </div>
         </div>
     );
